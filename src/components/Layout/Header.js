b45434280fb8c49548737692e5bac3e88beb5c7a
suresh patta <patta_suresh@optum.com>
2021-05-12 21:37:35 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import classes from "./Header.module.css";
+import mealsImage from "../../assests/meals.jpg";
+import HeaderCartButton from './HeaderCartButton';
+
+const header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart}/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default header;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHightlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHightlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHightlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
